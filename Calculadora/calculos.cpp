c37e7e9f7d6c90a08c18791dba7f8fa0e85a0c62
Alumno10807 <68626200+Alumno10807@users.noreply.github.com>
2020-08-07 00:06:34 -0500

    *Primer

---
+++ Calculadora/calculos.cpp
@@ -0,0 +1,22 @@
+#include "calculos.h"
+
+double calculos::Calculo(double x, char oper, double y)
+{
+    switch  (oper)
+    {
+    case '+':
+        return  x + y;
+
+    case '-':
+        return x - y;
+
+    case '*':
+        return  x * y; 
+
+    case '/':
+        return x / y;
+    default:
+        return 0.0; 
+    }
+        
+}
